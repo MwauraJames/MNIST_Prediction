6b28afdc6234b60eaf45ebc0a28c8609c55d522f
James Mwaura <jamesmwaura157@gmail.com>
2026-03-03 13:19:37 +0300

    fixed some errors during prediction

--- app.py
+++ app.py
@@ -12,34 +12,35 @@ photo = st.camera_input("Take a picture")
 
 def prepare_my_image(image):
     # 1. Load, grayscale, and resize
-        img = tf.keras.utils.load_img(
-            image,
-            color_mode="grayscale",
-            target_size=(28, 28)
-        )
-        img_array = tf.keras.utils.img_to_array(img)
+        # 1. Convert to grayscale
+    image = image.convert("L")
 
+    # 2. Resize to 28x28
+    image = image.resize((28, 28))
+
+    # 3. Convert to numpy array
+    img_array = np.array(image)
     # 2. Invert the colors (White paper -> Black, Dark ink -> White)
-        img_array = 255.0 - img_array
+    img_array = 255.0 - img_array
 
     # 3. Normalize to decimals (0.0 to 1.0)
-        img_array = img_array / 255.0
+    img_array = img_array / 255.0
 
     # --- 4. THE NEW FIX: THRESHOLDING ---
     # Any annoying gray background noise (below 0.5) gets crushed to pure black (0.0)
-        img_array = np.where(img_array < 0.5, 0.0, img_array)
+    img_array = np.where(img_array < 0.5, 0.0, img_array)
 
     # Any light gray ink gets boosted to pure bright white (1.0)
-        img_array = np.where(img_array > 0.5, 1.0, img_array)
+    img_array = np.where(img_array > 0.5, 1.0, img_array)
 
     # --- 5. VISUAL CHECK ---
     # Let's plot it right inside the function so you can immediately see if it worked
 
 
     # 6. Add the Batch dimension for the model
-        final_input = tf.expand_dims(img_array, axis=0)
+    final_input = tf.expand_dims(img_array, axis=0)
 
-        return final_input
+    return final_input
 
 
 if photo is not None:
@@ -47,7 +48,11 @@ if photo is not None:
     
     my_ready_image = prepare_my_image(image)
     # Convert to numpy
-  
-    
-    prediction = model.predict(my_ready_image)
-    st.write(prediction)
+if st.button("Predict"):
+    raw_predictions = model.predict(my_ready_image)
+    predicted_digit = np.argmax(raw_predictions)
+
+    confidence_decimal = np.max(raw_predictions)
+    confidence_percentage = confidence_decimal * 100
+
+    st.text(f"The model is {confidence_percentage:.2f}% sure this is a {predicted_digit}!")
